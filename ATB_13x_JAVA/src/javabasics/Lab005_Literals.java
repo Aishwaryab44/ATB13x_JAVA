0cbf12356393c80a344f5a23080f39351406b4aa
Dell <Dell@Aishwarya>
2025-10-11 22:12:24 +0530

    11oct

--- ATB_13x_JAVA/src/javabasics/Lab005_Literals.java
+++ ATB_13x_JAVA/src/javabasics/Lab005_Literals.java
@@ -4,20 +4,17 @@ public class Lab005_Literals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int age=65;
-int binary_num=0b1010; //add 0b at the start for base 10
-System.out.println(binary_num);
+		int age = 65;
+		int binary_num = 0b1010; // add 0b at the start for base 10
+		System.out.println(binary_num);
 
 //Char literal A-Z, a-z, !@#$%^^&*()__* etc
-char one= 'A';
-char two = '%';
+		char one = 'A';
+		char two = '%';
 
-/*
- * Escape characters 
- * \n -> newline 
- * \t-> tab line
- * \b -> backspace
- * \r-> carriage return
- */	}
+		/*
+		 * Escape characters \n -> newline \t-> tab line \b -> backspace \r-> carriage
+		 * return
+		 */ }
 
 }
